e32b5587bb0bab596512a488ec4e66af56b84f0d
novitasalsabila <novnovv10@gmail.com>
2023-03-16 16:47:32 +0700

    Menambahkan procedure inputan

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -7,5 +7,25 @@ int a[20];           // Deklarasi array a dengan ukuran 20
 int n;               // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
 
+void input() {        // Method untuk input
 
+    while (true) {   // Looping 
+
+        cout << "Masukan banyaknya elemen pada array : ";  // Output ke layar
+
+        cin >> n;     // Input dari pengguna
+
+        if (n <= 20)  // Jika n kurang dari atau sama dengan 20
+
+            break;    // Keluar dari loop
+
+        else {        // Jika n lebih dari 20
+
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // Output ke layar
+
+        }
+
+    }
+
+}
 
